88fb2f5100dad200626800a4eb1304c9e036a69e
Kim <kimbfsd@gmail.com>
2025-07-02 02:57:00 +0300

    svg logic + sidebar - top menu was implemented

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { SvgIcons } from '../svg-icons/svg-icons';
+import { CommonModule } from '@angular/common';
 
 // This component represents a sidebar in the application.
 // It includes a logout button that calls the logout method from the AuthService when clicked.
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgIcons, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   standalone: true
@@ -16,5 +18,23 @@ export class Sidebar {
   logout(): void {
     this.auth.logout();
   }
+
+  menuItems=[
+    {
+      lable: 'Home Page',
+      icon: 'home',
+      link: ''
+    },
+    {
+      lable: 'Chats',
+      icon: 'chat',
+      link: 'chats'
+    },
+    {
+      lable: 'Search',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
 }
 
