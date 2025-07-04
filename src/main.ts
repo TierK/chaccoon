92f587cff28d51149df3776b21cfb3373e32141a
Kim <kimbfsd@gmail.com>
2025-07-04 21:01:09 +0300

    some changes for deploy demo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-import '@fontsource/inter/800.css';
-import '@fontsource/inter/900.css';
-import '@fontsource/fira-code/400.css';
-import '@fontsource/fira-code/500.css';
-import '@fontsource/fira-code/600.css';
-import '@fontsource/fira-code/700.css';
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
